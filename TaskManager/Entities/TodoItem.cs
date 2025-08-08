aa191ddf71cffce5e7ffc4ab6d4dbc9abccadce4
Cesar Rodrigo Nieto Lopez <cesar_rnl@hotmail.com>
2025-08-07 19:56:09 -0600

    feat(todo): link TodoItem to creator IdentityUser
    Add CreatedByUserId and CreatedByUser navigation to TodoItem
    
    Add EF Core migration TasksUser (FK TodoItems.CreatedByUserId -> AspNetUsers.Id)
    
    Update ApplicationDbContextModelSnapshot

--- TaskManager/Entities/TodoItem.cs
+++ TaskManager/Entities/TodoItem.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace TaskManager.Entities;
 
@@ -11,7 +12,8 @@ public class TodoItem
     public string Description { get; set; }
     public int OrderIndex { get; set; }
     public DateTime CreatedAt { get; set; }
-    
+    public string CreatedByUserId { get; set; }
+    public IdentityUser CreatedByUser { get; set; }
     public List<Step> Steps { get; set; }
     public List<FileAttachment> FileAttachments { get; set; }
 }
---
+++ TaskManager/Migrations/20250808014919_TasksUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class TasksUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "TodoItems",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_CreatedByUserId",
+                table: "TodoItems",
+                column: "CreatedByUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_AspNetUsers_CreatedByUserId",
+                table: "TodoItems",
+                column: "CreatedByUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_AspNetUsers_CreatedByUserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_CreatedByUserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "TodoItems");
+        }
+    }
+}
